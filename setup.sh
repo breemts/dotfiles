c3e2f58dfbf52662bdf83ba7ec118fb5f80a0fd2
Benjamin Reemts <b.reemts@gmail.com>
2020-08-18 18:59:28 +0200

    move bashrc changes to .bashrc_extra which is deployed with setup.sh; more aliases

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# installs .bashrc_extra and modifies .bashrc
+sed -i -e 's/HISTSIZE=.*/HISTSIZE=100000/' $HOME/.bashrc
+sed -i -e 's/HISTFILESIZE=.*/HISTFILESIZE=200000/' $HOME/.bashrc
+sed -i -e 's/#force_color_prompt=yes/force_color_prompt=yes/' $HOME/.bashrc
+echo "[ -f $HOME/.bashrc_extra ] && source $HOME/.bashrc_extra" >> ${HOME}/.bashrc
