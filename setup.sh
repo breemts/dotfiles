bf395a428ebde125849d172fcfc4e9ad30fc3b49
Benjamin Reemts <b.reemts@gmail.com>
2020-09-14 17:06:36 +0200

    introduce function to check and install required packages

--- setup.sh
+++ setup.sh
@@ -2,12 +2,23 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+function check_and_install() {
+    if  [ "$UID" -eq 0 ] || sudo -v; then
+        if ! dpkg -s $1 > /dev/null ; then
+            echo "INFO: $1 not installed. Installing..."
+            sudo apt-get update && sudo apt-get install -yqq $1 && sudo rm -rf /var/lib/apt/lists/*
+        fi
+    else
+        echo "ERROR: can't install $1. Not root user rights" >&2
+    fi
+}
+
 # check if user is root (ID 0) or user is sudoer (sudo -l -U $(id -un))
 if  [ "$UID" -eq 0 ] || sudo -v; then
-    if ! dpkg -l curl jq wget ; then
-        echo "INFO: wget, jq or curl not installed. Installing..."
-        sudo apt update && sudo apt install -yqq curl jq wget && sudo rm -rf /var/lib/apt/lists/*
-    fi
+    check_and_install wget
+    check_and_install jq
+    check_and_install curl
+    
     tmp_dir=$(mktemp -d -t deb-XXXXXXXXXX)
     curl -s https://api.github.com/repos/sharkdp/fd/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*fd_.*amd64.deb").string' | wget -P $tmp_dir -qi -
     curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*bat_.*amd64.deb").string' | wget -P $tmp_dir -qi -
@@ -24,6 +35,8 @@ fi
 # Install fzf. maybe also get release?
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf \
 && $HOME/.fzf/install --all
+
+check_and_install bsdmainutils
 
 # installs .bashrc_extra and modifies .bashrc
 sed -i -e 's/HISTSIZE=.*/HISTSIZE=100000/' $HOME/.bashrc
