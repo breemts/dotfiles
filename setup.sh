173441a0c98436894c1e950de0447ce9d0db37f8
Benjamin Reemts <b.reemts@gmail.com>
2021-04-29 21:26:48 +0200

    install fd and bat for armhf

--- setup.sh
+++ setup.sh
@@ -2,6 +2,26 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+OS_TYPE="$(uname -m)"
+case "$OS_TYPE" in
+  x86_64|amd64)
+    OS_TYPE='amd64'
+    ;;
+  i?86|x86)
+    OS_TYPE='386'
+    ;;
+  aarch64|arm64)
+    OS_TYPE='arm64'
+    ;;
+  arm*)
+    OS_TYPE='arm'
+    ;;
+  *)
+    echo 'OS type not supported'
+    exit 2
+    ;;
+esac
+
 function check_and_install() {
     if  [ "$UID" -eq 0 ] || sudo -v; then
         if ! dpkg -s $1 > /dev/null ; then
@@ -21,11 +41,18 @@ if  [ "$UID" -eq 0 ] || sudo -v; then
     check_and_install curl
     
     tmp_dir=$(mktemp -d -t deb-XXXXXXXXXX)
-    curl -s https://api.github.com/repos/sharkdp/fd/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*fd_.*amd64.deb").string' | wget -P $tmp_dir -qi -
-    curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*bat_.*amd64.deb").string' | wget -P $tmp_dir -qi -
-    # if [ "$UID" -eq 0 ];then
-    #     dpkg -i $tmp_dir/*.deb
-    # else
+    case "$OS_TYPE" in
+        'amd64')
+            curl -s https://api.github.com/repos/sharkdp/fd/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*fd_.*amd64.deb").string' | wget -P $tmp_dir -qi -
+            curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*bat_.*amd64.deb").string' | wget -P $tmp_dir -qi -
+            ;;
+        'arm')
+            curl -s https://api.github.com/repos/sharkdp/fd/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*fd_.*armhf.deb").string' | wget -P $tmp_dir -qi -
+            curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | jq --raw-output '.assets[].browser_download_url | match(".*bat_.*armhf.deb").string' | wget -P $tmp_dir -qi -
+            ;;
+        *)
+            echo "OS Type ${OS_TYPE} not supported yet."
+    esac
     sudo dpkg -i $tmp_dir/*.deb
     rm -rf $tmp_dir
 else
@@ -38,6 +65,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf \
 && $HOME/.fzf/install --all
 
 # installs .bashrc_extra and modifies .bashrc
+echo "##### Modify .bashrc history and color settings #####"
 sed -i -e 's/HISTSIZE=.*/HISTSIZE=100000/' $HOME/.bashrc
 sed -i -e 's/HISTFILESIZE=.*/HISTFILESIZE=200000/' $HOME/.bashrc
 sed -i -e 's/#force_color_prompt=yes/force_color_prompt=yes/' $HOME/.bashrc
